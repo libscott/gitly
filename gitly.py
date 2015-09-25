87099c3e90d401e3c0dbe295e71cf28a2a7f4358
Scott Sadler <scott@alphanode.co>
2015-09-25 15:19:22 +0200

    a few working tests

---
+++ gitly.py
@@ -0,0 +1,199 @@
+import pygit2
+
+
+class Commit(object):
+    def __init__(self, repo, pygit2_commit):
+        self._commit = pygit2_commit
+        self.oid = pygit2_commit.oid
+        self.repo = repo
+
+    @property
+    def tree(self):
+        return Tree(self.repo, self._commit.tree)
+
+    def log(self):
+        log_ = self.repo.walk(self._commit.oid, pygit2.GIT_SORT_TIME)
+        return (Commit(self.repo, c) for c in log_)
+
+    def branch(self, name, force=False):
+        branch = self.repo.create_branch(name, self._commit, force)
+        return Branch(self.repo, branch.name)
+
+
+def empty_tree(repo):
+    empty = repo.TreeBuilder()
+    return repo.get(empty.write())
+
+
+EMPTY = object()
+
+
+class Tree(object):
+    def __init__(self, repo, pygit2_tree=None):
+        self.repo = repo
+        if pygit2_tree == None:
+            pygit2_tree = empty_tree(repo)
+        self._tree = pygit2_tree
+        self.oid = pygit2_tree.oid.hex
+
+    _ERROR = object()
+
+    def get(self, path, default=_ERROR):
+        parts = path.strip('/').split('/')
+        return reduce(lambda t, p: t._get(p, default), parts, self)
+
+    def _get(self, path, default=_ERROR):
+        if path in self._tree:
+            entry = self._tree[path]
+            if entry.filemode == pygit2.GIT_FILEMODE_BLOB:
+                blob = self.repo.get(entry.oid)
+                return blob.read_raw()
+            elif entry.filemode == pygit2.GIT_FILEMODE_TREE:
+                tree = self.repo.get(entry.oid)
+                return Tree(self.repo, tree)
+        if default is self._ERROR:
+            raise KeyError(path)
+        return default
+
+    __getitem__ = get
+
+    def subtree(self, path):
+        entry = self._tree[path]
+        if entry.filemode == pygit2.GIT_FILEMODE_TREE:
+            tree = self.repo.get(entry.oid)
+            return Tree(self.repo, tree)
+        raise KeyError(path)
+
+    def subtree_or_empty(self, path):
+        try:
+            return self.subtree(path)
+        except KeyError:
+            return Tree(self.repo)
+
+    def set(self, path, data):
+        """
+        Return a new tree inheriting from this tree, setting or updating
+        a given path.
+        """
+        parts = path.strip('/').split('/', 1)
+        key = parts.pop(0)
+
+        if parts:
+            data = self.subtree_or_empty(key).set(parts[0], data)
+
+        builder = self.repo.TreeBuilder(self.oid)
+
+        if data == EMPTY:
+            data = empty_tree(self.repo)
+        if data == None:
+            builder.remove(path)
+        elif type(data) in (Tree, pygit2.Tree):
+            builder.insert(key, data.oid, pygit2.GIT_FILEMODE_TREE)
+        else:
+            blob_id = self.repo.create_blob(data)
+            builder.insert(key, blob_id, pygit2.GIT_FILEMODE_BLOB)
+
+        tree = self.repo.get(builder.write())
+        return Tree(self.repo, tree)
+
+    def __contains__(self, path):
+        return path in self._tree
+
+    def __iter__(self):
+        return (e.name for e in self._tree)
+
+    def keys(self):
+        return list(self)
+
+    def __len__(self):
+        return sum(1 for _ in iter(self))
+
+    def __eq__(self, other):
+        if type(other) == type(self):
+            return self._tree.oid == other._tree.oid
+        return False
+
+
+def flatten_tree(tree, prefix=()):
+    """ Flatten a tree into name -> entry pairs """
+    for entry in tree._tree:
+        name = prefix + (entry.name,)
+        if entry.filemode == pygit2.GIT_FILEMODE_TREE:
+            subtree = tree.subtree(entry.name)
+            for item in flatten_tree(subtree, name):
+                yield item
+        else:
+            yield (name, entry)
+
+
+def dict_diff(dict1, dict2):
+    """
+    Combine a dictionary, discarding entries that are the same
+    in both left and right
+    """
+    tooid = lambda o: o.oid if o else None
+    for key in set(dict1.keys() + dict2.keys()):
+        item1 = dict1.get(key)
+        item2 = dict2.get(key)
+        if tooid(item1) != tooid(item2):
+            yield (key, (item1, item2))
+
+
+def tree_changes(tree1, tree2):
+    """ Iterate changes between 2 trees """
+    return dict_diff(dict(flatten_tree(tree1)), dict(flatten_tree(tree2)))
+
+
+def discover_head():
+    repodir = pygit2.discover_repository('.')
+    repo = pygit2.Repository(repodir)
+    return Commit(repo, repo.head.peel())
+
+
+class Branch(object):
+    """ Mutable branch object """
+    alice = pygit2.Signature('Alice Author', 'alice@authors.tld')
+    cecil = pygit2.Signature('Cecil Committer', 'cecil@committers.tld')
+
+    def __init__(self, repo, branch_name):
+        self.ref_name = branch_name
+        self.repo = repo
+        try:
+            ref = repo.lookup_reference(self.ref_name)
+            _tree = ref.peel().tree
+        except (ValueError, KeyError):
+            # The empty tree
+            _tree = empty_tree(repo)
+        self.tree = Tree(repo, _tree)
+
+    @classmethod
+    def discover(cls):
+        repodir = pygit2.discover_repository('.')
+        repo = pygit2.Repository(repodir)
+        return cls(repo, repo.head.name)
+
+    def __contains__(self, path):
+        return path in self.tree
+
+    def __getitem__(self, name):
+        out = self.tree
+        for part in name.split('/'):
+            out = out.get(part)
+        return out
+
+    def __setitem__(self, path, value):
+        self.tree = self.tree.set(path, value)
+
+    def __delitem__(self, path):
+        self[path] = None
+
+    def commit(self, msg, author=alice, committer=cecil):
+        ref = self.repo.lookup_reference(self.ref_name)
+        parent = ref.peel().id
+        self.repo.create_commit( self.ref_name
+                               , author
+                               , committer
+                               , msg
+                               , self.tree._tree.oid
+                               , [parent]
+                               )
---
+++ tests.py
@@ -0,0 +1,60 @@
+import os
+import os.path
+import unittest
+import shutil
+
+import pygit2
+
+from gitly import Tree, empty_tree
+
+
+TESTER = pygit2.Signature('Ted Tester', 'ted@authors.tld')
+
+TEST_REF_NAME = 'refs/head/test'
+
+
+class TestTree(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.repo_dir = os.path.join(os.path.dirname(__file__),
+                                    'build', 'test', 'repo')
+        if os.path.exists(cls.repo_dir):
+            shutil.rmtree(cls.repo_dir)
+        os.makedirs(cls.repo_dir)
+        cls.repo = pygit2.init_repository(cls.repo_dir, bare=True)
+
+    def setUp(self):
+        empty = empty_tree(self.repo)
+        if TEST_REF_NAME in self.repo.listall_references():
+            self.repo.lookup_reference(TEST_REF_NAME).delete()
+        commit = self.repo.create_commit(TEST_REF_NAME,
+                                         TESTER, TESTER, 'msg', empty.oid, [])
+
+    def test_init_empty_tree(self):
+        tree = Tree(self.repo)
+        self.assertEqual('4b825dc642cb6eb9a060e54bf8d69288fbee4904',
+                         str(tree.oid))
+
+    def test_tree_set(self):
+        tree = Tree(self.repo).set('a', 'b')
+        self.assertEqual('00264970afd30838f2a3b17376e05f8ca03b658b',
+                         str(tree.oid))
+
+
+    def test_init_some_tree(self):
+        tree = Tree(self.repo).set('a', 'a')
+        newtree = Tree(self.repo, tree._tree)
+        self.assertEqual('b39954843ff6e09ec3aa2b942938c30c6bd1629e',
+                         str(newtree.oid))
+
+    def test_set_subdir(self):
+        tree1 = Tree(self.repo)
+        tree1 = tree1.set('a/b', '1')
+        tree2 = Tree(self.repo)
+        subtree = Tree(self.repo).set('b', '1')
+        tree2.set('a', subtree)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
